b100f1e18238204a0d5bc098625599d037f79339
Avalos Enzo <avalos.enzo.g@gmail.com>
2021-09-07 17:13:12 -0300

    Implemented recyclerView on click listener Integrated recyclerView with data binding

--- app/src/main/java/com/example/pricerecorder/ProductAdapter.kt
+++ app/src/main/java/com/example/pricerecorder/ProductAdapter.kt
@@ -1,32 +1,26 @@
 package com.example.pricerecorder
 
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pricerecorder.database.Product
-import com.example.pricerecorder.generated.callback.OnClickListener
+import com.example.pricerecorder.databinding.ListItemProductBinding
 
 /*Adapta los datos usados a la recyclerView*/
-class ProductAdapter: ListAdapter<Product,RecyclerView.ViewHolder>(ProductDiffCallback) {
+class ProductAdapter(val clickListener: ProductListener): ListAdapter<Product,RecyclerView.ViewHolder>(ProductDiffCallback) {
 
     /*Crea los distintos viewholders usados, sera llamado cuando no haya viewholders existentes para reutilizar*/
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return ProductViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item_product,parent,false)
-        )
+        return ProductViewHolder.from(parent)
     }
 
     /*Metodo llamado para vincular las vistas no usadas con nuevos objetos y reciclarlas*/
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when(holder){
             is ProductViewHolder -> {
-                holder.bind(getItem(position))
+                holder.bind(getItem(position),clickListener)
             }
         }
     }
@@ -44,25 +38,27 @@ class ProductAdapter: ListAdapter<Product,RecyclerView.ViewHolder>(ProductDiffCa
     }
 
     /*La clase extiende de RecyclerView.ViewHolder ya que ese es el tipo de viewholder que se le especifico al adapter.*/
-    class ProductViewHolder constructor(itemView:View): RecyclerView.ViewHolder(itemView){
-        private val description:TextView = itemView.findViewById(R.id.product_description_text)
-        private val purchasePlace:TextView = itemView.findViewById(R.id.purchase_place_text)
-        private val price:TextView = itemView.findViewById(R.id.product_price_text)
-        private val updateDate:TextView = itemView.findViewById(R.id.update_date_text)
-        private val productImage:ImageView = itemView.findViewById(R.id.product_image)
-
+    class ProductViewHolder private constructor(val binding: ListItemProductBinding): RecyclerView.ViewHolder(binding.root){
         /*Responsable de unir cada objeto Producto a las vistas del layout*/
-        fun bind(product: Product){
-            description.text = product.description
-            purchasePlace.text = product.placeOfPurchase
-            price.text = product.price.toString()
-            updateDate.text = product.updateDate
-            productImage.setImageResource(R.drawable.ic_broken_image)
+        fun bind(product: Product,clickListener: ProductListener){
+            binding.product = product
+            binding.clickListener = clickListener
+            binding.executePendingBindings()
+        }
+
+        //Metodo de una clase que puede ser llamado sin necesidad de crear una instancia de la clase
+        companion object{
+            fun from(parent: ViewGroup): RecyclerView.ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemProductBinding.inflate(layoutInflater,parent,false)
+                return ProductViewHolder(binding)
+            }
         }
     }
 }
 
-/*Clase que implementa un click listener para los elementos de la lista*/
-class ProductListener(val clickListener: (productId :Long) -> Unit){
-    fun onClick(product: Product) = product.productId
+/*Clase que implementa un listener para cuando el usuario clickea sobre un elemento de la lista.
+* Recibe como parametro una expresion lambda.*/
+class ProductListener(val clickListener: (product:Product) -> Unit){
+    fun onClick(product: Product) = clickListener(product)
 }
---
+++ app/src/main/java/com/example/pricerecorder/homeFragment/BindingUtils.kt
@@ -0,0 +1,16 @@
+package com.example.pricerecorder.homeFragment
+
+import android.annotation.SuppressLint
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.pricerecorder.database.Product
+
+/*Metodo responsable de realizar las llamadas de framework necesarias para establecer valores*/
+//Indica que lint, debe ignorar cierta advertencia
+@SuppressLint("SetTextI18n")
+@BindingAdapter("productPriceString")
+fun TextView.setProductPriceString(item : Product?){
+    item?.let {
+        text = "$${item.price}"
+    }
+}
